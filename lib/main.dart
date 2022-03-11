9aca8d286ec4233e4c6284fa1af77c7a4fb4ce7a
Keyur Faldu <keyurfaldu001@gmail.com>
2022-03-11 10:02:28 +0530

    inital push
    - design feasty_ui

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:feasty/welcome/ui/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+import 'demo.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'GothamLight'),
+      title: 'Flutter Demo',
+      home: DEmoPage(),
+    );
+  }
+}
